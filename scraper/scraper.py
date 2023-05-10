a79b28a97fe2eeb004b3bb2a30293bd7b03d41eb
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-05-10 11:29:18 +0800

    Create scraper.py

---
+++ scraper/scraper.py
@@ -0,0 +1,11 @@
+# Basic web scrapper using BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.example.com"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+for link in soup.find_all("a"):
+    print(link.get("href"))
