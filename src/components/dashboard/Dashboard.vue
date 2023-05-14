81027ec2fa6b54a8102ca251bd522e1aefe204e8
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-05-14 22:19:13 +0800

    Added dashboard template

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,11 +1,68 @@
-<template>
-    <div>
-        This is a dashboard template
+<template lang="">
+    <div class="container-fluid px-5">
+      <br />
+      <div class="row">
+        <h2 class="text-start">Dashboard</h2>
+      </div>
+  
+      <div class="row mt-3">
+        <div class="col me-3 border border-grey rounded">
+          <h4 class="text-start pt-3">Overview</h4>
+          <hr />
+          <ul id="data_table" class="text-center">
+            Insert data table here
+          </ul>
+        </div>
+      </div>
+  
+      <div class="row mt-3">
+        <div class="col me-3 border border-grey rounded">
+          <h4 class="text-start pt-3">Market Salary</h4>
+          <hr />
+          <ul id="data_table" class="text-center">
+            Insert data table here
+          </ul>
+        </div>
+      </div>
+  
+      <div class="row mt-3">
+        <div class="col border border-grey rounded">
+          <h4 class="text-start pt-3">Job Relevance</h4>
+          <hr />
+          <ul id="data_table" class="text-center">
+            Insert data table here
+          </ul>
+        </div>
+      </div>
+  
+      <div class="row mt-3">
+        <div class="col border border-grey rounded">
+          <h4 class="text-start pt-3">Companies</h4>
+          <hr />
+          <ul id="data_table" class="text-center">
+            Insert data table here
+          </ul>
+        </div>
+      </div>
+      <br />
     </div>
-</template>
-
+  </template>
+  
 <script>
-export default{
-    name:"AppDashboard"
-}
+export default {
+    name: "dashboard",
+    data() {
+        return {
+
+        };
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+};
 </script>
+<style lang=""></style>
+  
