088829c4f174e314e46fea9fd7f3f67f6b51f593
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-05-14 14:19:46 +0800

    Rename home to dashboard and add route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 // Import Vue and VueRouter
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
+import Dashboard from '../components/dashboard/Dashboard.vue'
 
 
 // Define routes
 const routes = [
-  { path: '/home', component: Home }
+  { path: '/home', component: Home },
+  { path: '/dashboard', component: Dashboard },
 ]
 
 // Create the router
